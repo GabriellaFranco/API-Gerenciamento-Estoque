67927e55525a5c04fedddccc489622e99101c5ef
Gabriella Franco <gabriella-nena@hotmail.com>
2025-09-08 21:39:09 -0300

    GlobalExceptionHandler criado

---
+++ src/main/java/com/enterprise/gestaoestoque/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.enterprise.gestaoestoque.exception;
+
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@NoArgsConstructor
+public class ErrorResponse {
+
+    long status;
+    String message;
+    String timestamp;
+}
